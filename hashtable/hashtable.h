35d03cee45044b61261ad4fb95c0642ed81412e5
luca <luca_basilio@usp.br>
2023-12-23 21:15:48 -0300

    hashtable is now a single-file header library

--- hashtable/hashtable.h
+++ hashtable/hashtable.h
@@ -1,20 +1,19 @@
-#ifndef HASHTABLE_H
-#define HASHTABLE_H
+#ifndef CUP_HASHTABLE_H
+#define CUP_HASHTABLE_H
 
 #include <stdint.h>
-
-#include "cup/vector/vector.h"
 
 #define BUCKETS 11
 
 typedef struct {
+	struct hashtable_node_t* next;
 	char* key;
 	void* data;
 	uint64_t data_size;
-} hashtable_node_t ;
+} hashtable_node_t;
 
 typedef struct {
-	vector_t* buckets[BUCKETS];
+	hashtable_node_t* buckets[BUCKETS];
 	uint64_t bucket_count;
 } hashtable_t;
 
@@ -22,6 +21,84 @@ hashtable_t* hashtable_alloc();
 void hashtable_free(hashtable_t* ht);
 uint64_t hashtable_calc_hash(const char* key);
 void* hashtable_get(const hashtable_t* ht, const char* key);
-void  hashtable_set(const hashtable_t* ht, const char* key, const void* data, uint64_t data_size);
+void  hashtable_set(hashtable_t* ht, const char* key, const void* data, uint64_t data_size);
 
+#endif
+
+
+#ifdef CUP_HASHTABLE_IMPLEMENTATION
+
+uint64_t hashtable_calc_hash(const char* key)
+{
+	uint64_t hash = 0;
+	for(char* it = (char*)key; *it != 0; ++it)
+		hash = *it + 31 * hash;
+	return hash % BUCKETS;		
+}
+
+hashtable_t* hashtable_alloc()
+{
+	hashtable_t* ht = malloc(sizeof(hashtable_t));
+	ht->bucket_count = BUCKETS;
+	memset(ht->buckets, 0, sizeof(ht->buckets));
+	return ht;
+}
+
+void* hashtable_get(const hashtable_t* ht, const char* key)
+{
+	uint64_t index = hashtable_calc_hash(key);
+	hashtable_node_t* node = ht->buckets[index];
+	while (node) {
+		if (!strcmp(key, node->key))
+			return node->data;
+		node = node->next;
+	}
+
+	return NULL;
+}
+
+// When updating key values, you are responsible for freeing the previous object
+void hashtable_set(hashtable_t* ht, const char* key, const void* data, uint64_t data_size)
+{
+	uint64_t index = hashtable_calc_hash(key);
+	hashtable_node_t* last = NULL;
+	hashtable_node_t* node = ht->buckets[index];
+
+	// Search for key
+	while (node) {
+		if (!strcmp(key, node->key)) {
+			// Key found, replace data
+			goto CUP_HASHTABLE_UPDATE;
+		}
+		last = node;
+	}
+
+	// Add if not exists
+	node = malloc(sizeof(hashtable_node_t));
+	node->key = malloc(strlen(key));
+	strcpy(node->key, key);
+
+	if (last) last->next = node;
+	else ht->buckets[index] = node;
+
+CUP_HASHTABLE_UPDATE:
+	node->data_size = data_size;
+	node->data = malloc(data_size);
+	memcpy(node->data, data, data_size);
+}
+
+void hashtable_free(hashtable_t* ht)
+{
+	hashtable_node_t* previous;
+	hashtable_node_t* current;
+	for (uint64_t i = 0; i < ht->bucket_count; ++i) {
+		current = ht->buckets[i];
+		while (current) {
+			previous = current;
+			current = current->next;
+			free(previous);
+		}
+	}
+	free(ht);
+}
 #endif
--- hashtable/main.c
+++ hashtable/main.c
@@ -3,19 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "cup/hashtable/hashtable.h"
+#define CUP_STRUTILS_IMPLEMENTATION
 #include "cup/strutils/strutils.h"
 
-int main() {
+#define CUP_HASHTABLE_IMPLEMENTATION
+#include "hashtable.h"
+
+int main() 
+{
 	char str[] = "Nothing here but us chickens.";
 	hashtable_t* ht = hashtable_alloc();
 	size_t token_count;
 	char num_str[50];
 	char** tokens = cup_str_split(str, " ", &token_count);
-	for (size_t i = 0; i < token_count; ++i)
-	{
+	for (size_t i = 0; i < token_count; ++i) {
 		printf("%s\n", tokens[i]);
-		hashtable_set(ht, itoa(i, num_str, 10), tokens[i], strlen(tokens[i]) + 1);
+		snprintf(num_str, sizeof(num_str), "%d", i);
+		hashtable_set(ht, num_str, tokens[i], strlen(tokens[i]) + 1);
 	}
 
 	printf("%s\n", (char*)hashtable_get(ht, "0"));
